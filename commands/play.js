59f56bc443e7c8701944245452d76bd51a6e8873
Zardo <oziriz9@gmail.com>
2022-02-05 20:26:53 +0200

    main

--- commands/play.js
+++ commands/play.js
@@ -1,5 +1,5 @@
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType, AudioPlayerStatus, AudioPlayer } = require('@discordjs/voice');
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType, AudioPlayerStatus} = require('@discordjs/voice');
+const { SlashCommandBuilder} = require('@discordjs/builders');
 const ytdl = require('ytdl-core');
 const ytsr = require('ytsr');
 
@@ -9,10 +9,6 @@ module.exports = {
         .setDescription('Plays sickest songs')
         .addStringOption(option => option.setName('song').setDescription('Spowoky Owoky Pampukin').setRequired(true)),
     async execute(interaction) {
-        // const url = interaction.options.getString('url');
-        // console.log(url)
-
-
 
         const channel = interaction.member.voice.channel;
         const connection = joinVoiceChannel({
@@ -24,41 +20,30 @@ module.exports = {
         const input = interaction.options.getString('song');
         const player = (AudioPlayerStatus.Idle)
 
-        
         //player function
-        async function playit(ytLink,name) {
+           async function playit(ytLink) {
             const stream = ytdl(ytLink, { filter: 'audioonly' });
             const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
             const player = createAudioPlayer();
 
-            player.play(resource);
-            connection.subscribe(player);
-            player.on(AudioPlayerStatus.Playing, () => {
-                console.log('The audio player has started playing:');
-            });
+                player.play(resource);
+                connection.subscribe(player);
+                player.on(AudioPlayerStatus.Playing, () => {
+                    console.log('The audio player has started playing:');
+                });
 
-            await interaction.reply({ content: '***Now Playing:  ***' + ytLink, ephemeral: false });
+                await interaction.reply({ content: '***Now Playing:  ***' + ytLink, ephemeral: false });
 
         }
-        //sets idle after song ends
-        async function Timeout(lenght) {
-            setTimeout(() => {
-                AudioPlayerStatus.Idle
-            }, lenght);
-        }
 
-        // Link Maker
-        if (ytdl.validateURL(input)) {
+        // Link Makers
+        if (ytdl.getURLVideoID(input)) {
             await playit(input)
         }
 
-
-
         else {
             const searchResults = await ytsr(input, { limit: 1 }, { type: 'video' });
-            console.log(searchResults);
             await playit(searchResults.items[0].url);
-            await Timeout(searchResults.items[0].duration);
         }
     }
 }
