8d16ca84209f941b97b289a59cc3b9c636bc5968
Zardo <oziriz9@gmail.com>
2022-02-05 22:57:39 +0200

    main

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,12 @@
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType, AudioPlayerStatus} = require('@discordjs/voice');
 const { SlashCommandBuilder} = require('@discordjs/builders');
+const { Client, Collection, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const ytdl = require('ytdl-core');
 const ytsr = require('ytsr');
+require('dotenv').config()
+
+let queue = [];
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -18,32 +23,50 @@ module.exports = {
         });
 
         const input = interaction.options.getString('song');
-        const player = (AudioPlayerStatus.Idle)
+        const player = (AudioPlayerStatus.Idle);
+
+        async function quemanager(link) {
+            if (queue[0] == undefined) {
+                queue.push(link);
+                await playit ();
+            } else {
+                queue.push(link);
+                await interaction.reply({ content: '***Added to queue:  ***' + link, ephemeral: false })
+            }
+        }
+        
 
         //player function
-           async function playit(ytLink) {
-            const stream = ytdl(ytLink, { filter: 'audioonly' });
+           async function playit() {
+            const stream = ytdl(queue[0], { filter: 'audioonly' });
             const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
             const player = createAudioPlayer();
+
+            player.on(AudioPlayerStatus.Idle, () => checkQueue(queue));
+            const checkQueue = (queue) => {
+                queue.shift();
+                if (queue[0] != undefined) {
+                    playit ()
+                    
+                }
+            }
 
                 player.play(resource);
                 connection.subscribe(player);
                 player.on(AudioPlayerStatus.Playing, () => {
                     console.log('The audio player has started playing:');
                 });
-
-                await interaction.reply({ content: '***Now Playing:  ***' + ytLink, ephemeral: false });
-
+                interaction.reply({ content: '***Now Playing:  ***' + queue[0], ephemeral: false })
         }
 
         // Link Makers
         if (ytdl.getURLVideoID(input)) {
-            await playit(input)
+           await quemanager(input)
         }
 
         else {
             const searchResults = await ytsr(input, { limit: 1 }, { type: 'video' });
-            await playit(searchResults.items[0].url);
+            await quemanager(searchResults.items[0].url)
         }
     }
 }
